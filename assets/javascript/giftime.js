aa3d41ba2bfa7ed264dbba589180718938d166e5
tmac4403 <drive911@gmail.com>
2017-10-23 23:24:39 -0500

    3

--- assets/javascript/giftime.js
+++ assets/javascript/giftime.js
@@ -2,16 +2,13 @@ alert("hello");
 
 
     var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
-$("button").on("click", function() {
-      var person = $(this).attr("data-person");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=dc6zaTOxFJmzC&limit=10";
+
     $.ajax({
       url: queryURL,
       method: 'GET'
     }).done(function(response) {
       console.log(response);
   });
-    $("#sports-view").html("<h1>" + JSON.stringify(response) + "Sports Details</h1>");// new from Code School
+    $("#sports-view").html("<h1>" + JSON.stringify(response)) + "Sports Details</h1>";// new from Code School
 
-};
+});
